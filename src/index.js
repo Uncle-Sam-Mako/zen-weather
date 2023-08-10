bb690d551c8ebaf8bba16686cf5da9cffa6bc545
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2023-08-10 19:36:20 +0200

    Retrieve current weather infos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import "./css/main.scss"
+import axios from 'axios';
+
+
+function formatDateTime(datetime) {
+    const inputDate = datetime;
+
+    // Convertir la chaîne de date en objet Date
+    const dateObject = new Date(inputDate);
+
+    // Obtenir le jour de la semaine et le mois sous forme abrégée
+    const options = { weekday: 'short', month: 'short', day: 'numeric' };
+    const formattedDate = new Intl.DateTimeFormat('en-US', options).format(dateObject);
+
+    // Obtenir l'heure et les minutes
+    const hours = dateObject.getHours();
+    const minutes = dateObject.getMinutes();
+
+    // Formater l'heure
+    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+
+    return [formattedDate, formattedTime]
+}
+
+const currentWeather = document.getElementById('current_weather') ;
+
+const url = 'http://api.weatherapi.com/v1/current.json?key=f9de0c04c1bc48c2a6485330230408&q=London&aqi=no'
+
+axios.get('http://api.weatherapi.com/v1/current.json?key=f9de0c04c1bc48c2a6485330230408&q=London&aqi=no')
+.then(response => {
+    
+    const location = response.data.location;
+    const weather = response.data.current;
+    const condition = weather.condition;
+
+    currentWeather.querySelector('.location').textContent = `${location.name}, ${location.country}`;
+    currentWeather.querySelector('.localdate').textContent = formatDateTime(location.localtime)[0];
+    currentWeather.querySelector('.temp_hour .hour').textContent = formatDateTime(location.localtime)[1];
+
+    currentWeather.querySelector('.temp_hour .temp').textContent = `${parseInt(weather.temp_c)}°`;
+
+    currentWeather.querySelector('.condition').textContent = condition.text
+    currentWeather.querySelector('.cloud_icon').src = condition.icon
+
+    console.log(response.data)
+})
+.catch(error => {
+    console.error(error)
+})
